3b9be248fbbf60c48edb371f667b6984a26c47d0
rahmadanyft <103974897+OkumaDGreat@users.noreply.github.com>
2023-04-06 00:33:41 +0700

    Update footer.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="sticky-footer bg-white">
   <div class="container my-auto">
     <div class="copyright text-center my-auto">
-      <span>Copyright &copy; radisyah</span>
+      <span>Copyright &copy; rahman</span>
     </div>
   </div>
 </footer>
